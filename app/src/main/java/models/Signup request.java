b60d47f5f3b6b71a5c84f184c517836414e4581e
adithyareddy369-Byte <adithyapreddy768@gmail.com>
2025-12-04 22:13:48 +0530

    Add SignupRequest model with constructor

--- app/src/main/java/models/Signup request.java
+++ app/src/main/java/models/Signup request.java
@@ -1,1 +1,14 @@
+package com.tracetrail.mobile.models;
 
+public class SignupRequest {
+    private String email;
+    private String username;
+    private String password;
+
+    public SignupRequest(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+    // getters/setters
+}
