98fe09a66d011ed054f49e10baf26b460bb470fe
adithyareddy369-Byte <adithyapreddy768@gmail.com>
2025-12-04 22:20:09 +0530

    A

--- app/src/main/java/viewmodels/Analyzeviewmodel.java
+++ app/src/main/java/viewmodels/Analyzeviewmodel.java
@@ -1,1 +1,7 @@
+package com.tracetrail.mobile.viewmodels;
 
+import androidx.lifecycle.ViewModel;
+
+public class AnalyzeViewModel extends ViewModel {
+    // Handle analysis requests/responses with LiveData
+}
