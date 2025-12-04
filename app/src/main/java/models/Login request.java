860461d8ccc54c42587c3086ef3043260e5528f3
adithyareddy369-Byte <adithyapreddy768@gmail.com>
2025-12-04 22:13:31 +0530

    Create LoginRequest class for user login
    Added LoginRequest class with email and password fields.

--- app/src/main/java/models/Login request.java
+++ app/src/main/java/models/Login request.java
@@ -1,1 +1,12 @@
+package com.tracetrail.mobile.models;
 
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    // getters/setters
+}
